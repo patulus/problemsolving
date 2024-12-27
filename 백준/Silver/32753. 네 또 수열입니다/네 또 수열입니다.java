ef788e453444c240797d672c95f41588cf234747
PARK, Yeonjong <calvanasine@outlook.com>
2024-12-27 13:19:34 +0900

    [Silver IV] Title: 네 또 수열입니다, Time: 144 ms, Memory: 16436 KB -BaekjoonHub

--- 백준/Silver/32753. 네 또 수열입니다/네 또 수열입니다.java
+++ 백준/Silver/32753. 네 또 수열입니다/네 또 수열입니다.java
@@ -3,7 +3,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 
 public final class Main {
     public static void main(String[] args) throws IOException {
@@ -15,51 +14,19 @@ public final class Main {
         int N = Integer.parseInt(userInput[0]);
         int K = Integer.parseInt(userInput[1]);
 
-        int[] seq = new int[N*K];
-        int[] res = new int[N*K];
-        int[] kary = new int[N];
-        Arrays.fill(kary, K);
-
-        makeSeq(seq, kary, res, N, K, 0);
-        if (res[0] != 0) {
-            for (int i = 0; i < N * K; ++i) {
-                sb.append(res[i]).append(" ");
-            }
-            sb.append("\n");
-            bw.write(sb.toString());
+        if (N == 1) {
+            while (K-- > 0) sb.append("1 ");
+        } else if (N == 2 && K == 1) {
+            sb.append("1 2");
         } else {
-            bw.write("-1\n");
+            sb.append("-1");
         }
+        sb.append("\n");
+
+        bw.write(sb.toString());
         bw.flush();
 
         bw.close();
         br.close();
-    }
-
-    private static void makeSeq(int[] seq, int[] kary, int[] res, int N, int K, int idx) {
-        if (idx == N * K) {
-            for (int i = 0; i < res.length; ++i) {
-                res[i] = seq[i];
-            }
-            return;
-        }
-
-        for (int i = 0; i < N; ++i) {
-            if (kary[i] <= 0) continue;
-
-            int sum = 0;
-            for (int j = 0; j < idx; ++j) {
-                sum += seq[j];
-            }
-            if (idx < N * K - 1 && sum + (i + 1) != idx + 1) continue;
-
-            seq[idx] = i + 1;
-            --kary[i];
-
-            makeSeq(seq, kary, res, N, K, idx + 1);
-
-            seq[idx] = 0;
-            ++kary[i];
-        }
     }
 }
