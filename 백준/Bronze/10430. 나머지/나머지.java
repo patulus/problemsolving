6936659c886e4b0ff6c5c11767697f07e7910f18
PARK, Yeonjong <yeonjong.park@kumoh.ac.kr>
2025-09-17 10:13:39 +0900

    [Bronze V] Title: 나머지, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        String[] tokens = br.readLine().split(" ");
+        int A = Integer.parseInt(tokens[0]);
+        int B = Integer.parseInt(tokens[1]);
+        int C = Integer.parseInt(tokens[2]);
+
+        sb.append((A + B) % C).append("\n");
+        sb.append(((A % C) + (B % C)) % C).append("\n");
+        sb.append((A * B) % C).append("\n");
+        sb.append(((A % C) * (B % C)) % C).append("\n");
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
